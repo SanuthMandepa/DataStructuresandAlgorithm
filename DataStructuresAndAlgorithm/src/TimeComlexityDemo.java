d769f65dd33fcfdcd095357208081935387132e3
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-04-04 12:20:33 +0530

    Time complexity 1

---
+++ DataStructuresAndAlgorithm/src/TimeComlexityDemo.java
@@ -0,0 +1,13 @@
+public class TimeComlexityDemo {
+    public static void main(String[] args){
+        double now = System.currentTimeMillis();
+        TimeComlexityDemo demo = new TimeComlexityDemo();
+        System.out.println(demo.findSum(9999999));
+
+        System.out.println("Time taken- " + (System.currentTimeMillis()- now)+ " millisecs.");
+
+    }
+    public int findSum(int n){
+        return n * (n+1)/2;
+    }
+}
