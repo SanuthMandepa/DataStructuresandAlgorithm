7a87e76b318300662f128684a390df992d39b9cb
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-05-22 12:19:43 +0530

    check

--- DataStructuresAndAlgorithm/src/TimeComlexityDemo.java
+++ DataStructuresAndAlgorithm/src/TimeComlexityDemo.java
@@ -10,11 +10,6 @@ public class TimeComlexityDemo {
 //    public int findSum(int n){
 //        return n * (n+1)/2;
 //    }
-    public int findSum(int n){
-        int sum = 0;
-        for(int i = 1; i <=n; i++){
-            sum =sum +i;
-        }
-        return sum;
+
     }
 }
