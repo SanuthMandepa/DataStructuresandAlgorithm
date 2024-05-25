293738e759aecefe7186555e61964274f588449f
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-05-25 12:24:29 +0530

    Print An Array

--- DataStructuresAndAlgorithm/src/ArrayUtil.java
+++ DataStructuresAndAlgorithm/src/ArrayUtil.java
@@ -17,11 +17,21 @@ public class ArrayUtil {
         myArray[2] = 8;
         myArray[3] = 7;
         myArray[4] = 4;
+
         printArray(myArray);
     }
 
     public static void main (String[] args){
         ArrayUtil arrUtil = new ArrayUtil();
         arrUtil.arrayDemo();
+        ArrayUtil aarPrint = new ArrayUtil();
+        aarPrint.printNewArray(new int[] {2,4,6,8,10});
+    }
+    public void printNewArray(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i <n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
